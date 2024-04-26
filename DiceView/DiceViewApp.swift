7b33af9178e419ab82b907dfa697823146b3a6e9
Christian Manzaraz <magnusmanz@gmail.com>
2024-04-26 12:04:30 -0300

    Initial Commit

---
+++ DiceView/DiceViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceViewApp.swift
+//  DiceView
+//
+//  Created by Christian Manzaraz on 26/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct DiceViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
